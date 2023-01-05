72e7b10b7677b50fab077149c4049a46a0be2643
artchitector <i@nkuhta.ru>
2023-01-05 21:01:04 +0300

    God bless 33th commit! --- Artist make paintings with invoke.AI, upsale factor 2

---
+++ artist/ecosystem.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apps : [{
+    name: "artist",
+    script: "main.py",
+    instances: '1', // Or a number of instances
+    interpreter: 'C:\\Users\\Никсон\\AppData\\Local\\Programs\\Python\\Python311\\python.exe'
+  }]
+};
--- artist/main.py
+++ artist/main.py
@@ -1,4 +1,7 @@
 import io
+import os
+import re
+import subprocess
 
 from flask import Flask, render_template, Response, abort, request
 from io import BytesIO
@@ -6,24 +9,58 @@ from PIL import Image
 
 app = Flask(__name__)
 
+
 @app.route('/painting', methods=['POST'])
 def painting():
     print(request.form['idea'])
     print(request.form['tags'])
     print(request.form['seed'])
 
-    # with open('allah.jpg', mode='rb') as file:
-    #     fileContent = file.read()
-    #     return Response(fileContent, content_type="image/jpeg")
+    filename = getPaintingFromInvokeAIFilename()
 
-    im = Image.open("allah.jpg")
+    im = Image.open(filename)
     print(im.format, im.size, im.mode)
-    out = im.resize((512, 512), resample=True)
 
     img_byte_arr = io.BytesIO()
-    out.save(img_byte_arr, format="JPEG")
+    im.save(img_byte_arr, format="JPEG")
 
     return Response(img_byte_arr.getvalue(), content_type="image/jpeg")
+
+
+def getPaintingFromInvokeAIFilename():
+    prepareFileForInvokeAI()
+
+    pattern = re.compile("^.*(C:\\\\invokeai\\\\outputs\\\\[0-9\.]+\.png+).*$")
+    filename = None
+
+    # print("###START running invoke.py")
+    ret = os.popen(
+        'chcp 437 & C:\invokeai\.venv\Scripts\python.exe C:\invokeai\.venv\Scripts\invoke.py --from_file "C:\invokeai\list.txt"')
+    # print("### GOT RESPONSE")
+    lines = ret.readlines()
+    for line in lines:
+        match = pattern.match(line)
+        if match is not None:
+            filename = match.groups()[0]
+            print(f"Found filename: {filename}")
+
+    if filename is not None:
+        return filename
+    else:
+        raise Exception("filename not found")
+
+
+def prepareFileForInvokeAI():
+    idea = request.form['idea']
+    tags = request.form['tags']
+    seed = request.form['seed']
+    lines = []
+    lines.append(f'{idea},{tags} -S{seed} -W512 -H768 -s50 -U2')
+    with open("C:\invokeai\list.txt", "w") as text_file:
+        for line in lines:
+            text_file.write(line)
+    text_file.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8083, debug=True)
