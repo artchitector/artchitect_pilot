1158660cdba9d107d3d17fe6788ec2c63bec486e
artchitector <i@nkuhta.ru>
2023-01-11 21:32:13 +0300

    Revert "God bless 50th commit!"
    This reverts commit 838b6080ab55793e3a82209030ddb8ec0c2b39b2.

--- eye/layouts/default.vue
+++ eye/layouts/default.vue
@@ -1,21 +1,5 @@
 <template>
   <div>
-    <!-- Yandex.Metrika counter -->
-    <script type="text/javascript" >
-      (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-        m[i].l=1*new Date();
-        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-      (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-      ym(92022377, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true
-      });
-    </script>
-    <noscript><div><img src="https://mc.yandex.ru/watch/92022377" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-    <!-- /Yandex.Metrika counter -->
     <div class="has-text-centered is-header pt-1 pb-2">
       <div>
         <NuxtLink :to="{name: 'index'}">
@@ -39,11 +23,8 @@
 
     <footer class="footer">
       <p class="has-text-centered">
-        github: <a href="https://github.com/artchitector/artchitect" target="_blank">https://github.com/artchitector/artchitect</a><br/>
-        telegram: <a href="https://t.me/artchitect_space" target="_blank">t.me/artchitect_space</a><br/>
-        twitter: <a href="https://twitter.com/artchitector" target="_blank">@artchitector</a>
+        github: <a href="https://github.com/artchitector/artchitect" target="_blank">https://github.com/artchitector/artchitect</a>
       </p>
-
     </footer>
   </div>
 </template>
