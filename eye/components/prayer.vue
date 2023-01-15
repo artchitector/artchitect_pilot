5aca770deeff08920a4de987b50ee2fabcb4cd8a
artchitector <i@nkuhta.ru>
2023-01-15 03:02:20 +0300

    God bless 73th commit! --- pray implemented as inside a temple

---
+++ eye/components/prayer.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="box">
+    <div v-if="!visible" class="has-text-centered">
+      <button class="button is-success" @click="visible=true">To pray!</button>
+    </div>
+    <div v-else>
+      <textarea class="textarea" v-model="pray"
+                placeholder="Place for pray message. Write carefully. Between you and God. Secure, data burns and not being send anywhere."></textarea>
+      <div class="has-text-centered mt-2"><button class="button" @click="clear()">Pray! (burn text)</button></div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      visible: false,
+      pray: ""
+    }
+  },
+  methods: {
+    clear() {
+      this.pray = ""
+    }
+  }
+}
+</script>
--- eye/pages/index.vue
+++ eye/pages/index.vue
@@ -3,10 +3,11 @@
     <section class="green-section has-text-centered">
       <p>
         🚀 <b>Artchitect</b> launched 15th Jan 2023!<br/>
-        <a class="button is-success mt-2" href="#cards">Go to cards</a>
+        <a class="button is-success mt-2" href="#cards">Go to cards</a><br/>
       </p>
     </section>
     <description/>
+    <prayer/>
     <section>
       <last-artworks/>
     </section>
@@ -15,9 +16,10 @@
 <script>
 import LastArtworks from '@/components/last-cards/last-artworks.vue'
 import Description from "@/components/description/description";
+import Prayer from "@/components/prayer";
 
 export default {
-  components: {Description, LastArtworks},
+  components: {Description, LastArtworks, Prayer},
   layout: 'artchitect',
   head: {
     title: 'Artchitect'
