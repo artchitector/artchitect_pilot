efac737919cc41355a0e6cefe897585c6bcb7957
artchitector <i@nkuhta.ru>
2023-01-21 22:10:10 +0300

    God bless 101th commit! --- init-db.sql updated

--- scripts/pg_config/init-db.sql
+++ scripts/pg_config/init-db.sql
@@ -1,5 +1,14 @@
--- Run it from root (postgres) user.
+-- Run it from root
+sudo -u postgres psql -- create DB
 create database artchitect;
 create user artchitector with encrypted password 'Zz123456';
 ALTER DATABASE artchitect OWNER TO artchitector;
+sudo -u postgres psql -d artchitect
 grant all privileges on database artchitect to artchitector;
+
+-- # dumps
+-- backup
+pg_dump -d artchitect -U artchitector -h localhost -p 21431 | gzip > dump20230120.sql.gz
+-- restore
+gunzip dump20230120.sql.gz
+psql -U artchitector -d artchitect -h localhost -p 21431 -f dump20230120.sql -v ON_ERROR_STOP=1
