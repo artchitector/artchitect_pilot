ccec671caaf0e7d9dbe36a72fc0ed5146fd81e2e
artchitector <i@nkuhta.ru>
2023-01-22 11:18:33 +0300

    God bless 107th commit! --- gate pm2

--- gate/deploy.sh
+++ gate/deploy.sh
@@ -1,4 +1,2 @@
 go build -o ./bin/gate cmd/main.go
-# services manual https://luci7.medium.com/golang-running-a-go-binary-as-a-systemd-service-on-ubuntu-18-04-in-10-minutes-without-docker-e5a1e933bb7e
-# see service config in scripts/gate/gate.service
-service gate restart
+pm2 start
---
+++ gate/ecosystem.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  apps : [{
+    name: "gate",
+    script: "./bin/gate",
+    instances: '1'
+  }]
+};
