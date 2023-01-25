31ff3c4943469f0691cc58b9fe0e65b1b27f08e9
artchitector <i@nkuhta.ru>
2023-01-25 12:55:24 +0300

    God bless 126th commit! --- promo for Artchitect Infinite

--- scripts/pg_config/init-db.sql
+++ scripts/pg_config/init-db.sql
@@ -1,14 +1,20 @@
 -- Run it from root
-sudo -u postgres psql -- create DB
+
+-- sudo -u postgres psql -- create DB
 create database artchitect;
 create user artchitector with encrypted password 'Zz123456';
 ALTER DATABASE artchitect OWNER TO artchitector;
-sudo -u postgres psql -d artchitect
+-- sudo -u postgres psql -d artchitect
 grant all privileges on database artchitect to artchitector;
 
 -- # dumps
+--     on database server create .pgpass file with contents:
+--    hostname:port:database:username:password
+--    localhost:21431:artchitect:artchitector:***
+--    chmod 600 ~/.pgpass
+
 -- backup
-pg_dump -d artchitect -U artchitector -h localhost -p 21431 | gzip > dump20230120.sql.gz
+--      pg_dump -d artchitect -U artchitector -h localhost -p 21431 | gzip > dump20230120.sql.gz
 -- restore
-gunzip dump20230120.sql.gz
-psql -U artchitector -d artchitect -h localhost -p 21431 -f dump20230120.sql -v ON_ERROR_STOP=1
+--      gunzip dump20230120.sql.gz
+--      psql -U artchitector -d artchitect -h localhost -p 21431 -f dump20230120.sql -v ON_ERROR_STOP=1
