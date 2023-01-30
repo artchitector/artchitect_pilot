f467409f2ab7ba3dffaf23ab4a6c4ead11b39131
artchitector <i@nkuhta.ru>
2023-01-30 22:56:50 +0300

    God bless 155th commit! --- watermark started

--- gate/go.mod
+++ gate/go.mod
@@ -24,6 +24,7 @@ require (
 	github.com/go-playground/validator/v10 v10.11.1 // indirect
 	github.com/go-redis/redis/v8 v8.11.5 // indirect
 	github.com/goccy/go-json v0.10.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.13.0 // indirect
@@ -46,9 +47,10 @@ require (
 	github.com/stretchr/testify v1.8.1 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	golang.org/x/crypto v0.4.0 // indirect
+	golang.org/x/image v0.3.0 // indirect
 	golang.org/x/net v0.4.0 // indirect
 	golang.org/x/sys v0.3.0 // indirect
-	golang.org/x/text v0.5.0 // indirect
+	golang.org/x/text v0.6.0 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- gate/handler/card.go
+++ gate/handler/card.go
@@ -1,9 +1,19 @@
 package handler
 
 import (
+	"bytes"
 	"github.com/artchitector/artchitect/gate/resizer"
 	"github.com/gin-gonic/gin"
+	"github.com/golang/freetype"
+	"github.com/golang/freetype/truetype"
+	"github.com/pkg/errors"
 	"github.com/rs/zerolog/log"
+	"golang.org/x/image/draw"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/gofont/goregular"
+	"golang.org/x/image/math/fixed"
+	"image"
+	"image/jpeg"
 	"net/http"
 )
 
@@ -63,9 +73,17 @@ func (ch *CardHandler) HandleImage(c *gin.Context) {
 	if err != nil {
 		log.Error().Err(err).Msgf("[card_controller:HandleImage] failed to get cached image")
 	} else {
-		c.Data(http.StatusOK, "image/jpeg", cached)
+		var newImg []byte
+		newImg, err = ch.watermark(cached)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.Data(http.StatusOK, "image/jpeg", newImg)
 		return
 	}
+
+	log.Fatal().Msgf("STOP")
 
 	card, found, err := ch.cardsRepository.GetCardWithImage(c, request.ID)
 	if err != nil {
@@ -82,4 +100,52 @@ func (ch *CardHandler) HandleImage(c *gin.Context) {
 		return
 	}
 	c.Data(http.StatusOK, "image/jpeg", img)
+}
+
+func (ch *CardHandler) watermark(imgB []byte) ([]byte, error) {
+	text := "2000"
+	r := bytes.NewReader(imgB)
+	img, err := jpeg.Decode(r)
+	if err != nil {
+		return []byte{}, errors.Wrap(err, "[watermark] failed to decode jpeg")
+	}
+	rgbaImg := image.NewRGBA(img.Bounds())
+	draw.Draw(rgbaImg, img.Bounds(), img, image.Point{}, draw.Src)
+
+	fontFace, err := freetype.ParseFont(goregular.TTF)
+	if err != nil {
+		return []byte{}, errors.Wrapf(err, "[watermark] failed get face font")
+	}
+	fontDrawer := font.Drawer{
+		Dst: rgbaImg,
+		Src: image.White,
+		Face: truetype.NewFace(fontFace, &truetype.Options{
+			Size:    128.0,
+			Hinting: font.HintingFull,
+		}),
+	}
+	// calculate
+	textBounds, _ := fontDrawer.BoundString(text)
+	xPosition := (fixed.I(rgbaImg.Rect.Max.X) - fontDrawer.MeasureString(text)) / 2
+	log.Info().Msgf(
+		"[watermark] maxX: %d, measure: %s, result: %s",
+		rgbaImg.Rect.Max.X,
+		fontDrawer.MeasureString(text).String(),
+		xPosition.String(),
+	)
+	textHeight := textBounds.Max.Y - textBounds.Min.Y
+	yPosition := fixed.I((rgbaImg.Rect.Max.Y)-textHeight.Ceil())/2 + fixed.I(textHeight.Ceil())
+	fontDrawer.Dot = fixed.Point26_6{
+		X: xPosition,
+		Y: yPosition,
+	}
+
+	fontDrawer.DrawString(text)
+	log.Info().Msgf("[watermark] draw success")
+	buf := new(bytes.Buffer)
+	if err := jpeg.Encode(buf, rgbaImg, nil); err != nil {
+		return []byte{}, errors.Wrapf(err, "[watermark] failed to encode jpeg")
+	}
+
+	return buf.Bytes(), nil
 }
