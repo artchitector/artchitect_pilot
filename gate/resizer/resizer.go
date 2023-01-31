e4bbeb0f9d2451d73d18b19ea8e1fca300038955
artchitector <i@nkuhta.ru>
2023-01-31 03:17:30 +0300

    God bless 159th commit! --- more quality in resizer

--- gate/resizer/resizer.go
+++ gate/resizer/resizer.go
@@ -19,6 +19,7 @@ func Resize(rawImg []byte, size string) ([]byte, error) {
 	}
 
 	var height, width uint
+	var quality int
 	switch size {
 	case model.SizeXF:
 		// nothing to do, image already full
@@ -26,15 +27,19 @@ func Resize(rawImg []byte, size string) ([]byte, error) {
 	case model.SizeF:
 		width = uint(1024)
 		height = uint(1536)
+		quality = 90
 	case model.SizeM:
 		width = uint(512)
 		height = uint(768)
+		quality = 80
 	case model.SizeS:
 		width = uint(256)
 		height = uint(384)
+		quality = 80
 	case model.SizeXS:
 		width = uint(128)
 		height = uint(192)
+		quality = 80
 	default:
 		// TODO сделать из этого ответ bad-requst, если такое пришло
 		return []byte{}, errors.Errorf("[resizer] wrong size %s", size)
@@ -42,7 +47,7 @@ func Resize(rawImg []byte, size string) ([]byte, error) {
 
 	img = resize.Resize(width, height, img, resize.Lanczos3)
 	buf := new(bytes.Buffer)
-	if err := jpeg.Encode(buf, img, nil); err != nil {
+	if err := jpeg.Encode(buf, img, &jpeg.Options{Quality: quality}); err != nil {
 		return []byte{}, errors.Wrapf(err, "[resizer] failed to encode jpeg")
 	}
 	log.Info().Msgf(
