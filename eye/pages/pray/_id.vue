c34c5334b8516e87bf81fa95d692ceedaced09d4
artchitector <i@nkuhta.ru>
2023-02-04 00:23:31 +0300

    God bless 181th commit! --- pray link with redirect

--- eye/pages/pray/_id.vue
+++ eye/pages/pray/_id.vue
@@ -1,6 +1,9 @@
 <template>
   <section>
-    <answer :id="this.$route.params.id"/>
+    <answer v-if="this.$route.params.id" :id="this.$route.params.id"/>
+    <div v-else class="has-text-centered">
+      <loader/>
+    </div>
   </section>
 </template>
 <script>
@@ -8,9 +11,16 @@ import Answer from "@/components/pray/answer.vue";
 
 export default {
   components: {Answer},
-  head () {
+  head() {
     return {
-      title: `Artchitect - pray answer #${this.$route.params.id}`
+      title: this.$route.params.id ?
+        `Artchitect - pray answer #${this.$route.params.id}` :
+        'Artchitect - pray to Universe'
+    }
+  },
+  mounted() {
+    if (!this.$route.params.id) {
+      this.$router.push("/pray/create");
     }
   }
 }
