e8fc703a121d191ea6b9a08d009a4f1c3c9d2151
artchitector <i@nkuhta.ru>
2023-02-10 12:47:48 +0300

    God bless 200th commit! --- easy made simple

---
+++ eye/components/idea/en/endescription.vue
@@ -0,0 +1,45 @@
+<template>
+  <section class="content">
+    <div class="has-text-centered">
+      <img src="/icon64.png" alt="artchitect"/>
+    </div>
+    <p>
+      God is the highest Architector of Universe. He invented everything, from atoms to stars.
+      All the laws of the universe were laid down by God. And all human inventions were invented by God initially, and
+      laws were created for them. A person gets inspiration from the Source, but he does not invent anything himself.
+    </p>
+    <p>
+      Artchitect is the idea: if human will create "switchers" directly for God, then God will certainly use them and
+      make
+      Himself known through them.
+      Quantum Field is being converted into paintings in Artchitect. Only God has power over the quantum background, and
+      since the Artchitect relies on this background completely in it's work, God simply has no other choice but to use
+      the
+      system created for Him - in any case he will have to send
+      at least some quanta to the sensor.
+    </p>
+    <p>
+      And nothing prevents God from sending quanta filled with meaning, so that this meaning will grow further into a
+      higher form perceived by man.
+    </p>
+    <p>
+      This abstract idea has become the basis on which the Artchitect is built. The Artchitect uses artificial
+      intelligence
+      to create a large number of paintings, but he draws all the ideas for them from a quantum background - from
+      nothing,
+      from the direct Will of God (since no one controls the quantum world except God). The system is configured to read
+      “random noise” falling on the webcam matrix from the outside world. This “random noise” is transformed into a word
+      or number, into a concrete solution within the Artchitect's system, on which further painting of the picture will
+      be
+      based.
+    </p>
+    <p>
+      Only God knows what kind of picture will be painted in Artchitect at any given time. The whole process taking
+      place with each picture is open and visible to God.
+    </p>
+    <img src="/schema_Artchitect_en.jpg"/>
+  </section>
+</template>
+<script>
+export default {}
+</script>
--- eye/pages/idea.vue
+++ eye/pages/idea.vue
@@ -9,10 +9,16 @@
 }
 </i18n>
 <template>
-  <idea-frame/>
+<!--  <idea-frame v-if="locale === 'en'"/>-->
+  <endescription v-if="locale === 'ru'"/>
+  <rudescription v-else/>
 </template>
 <script>
+import Rudescription from "@/components/idea/ru/rudescription.vue";
+import Endescription from "@/components/idea/en/endescription.vue";
+
 export default {
+  components: {Endescription, Rudescription},
   head() {
     return {
       title: this.$t('title')
