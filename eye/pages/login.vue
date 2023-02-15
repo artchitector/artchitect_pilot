317721fc22683ca83d24932c23838083b24b42e5
artchitector <i@nkuhta.ru>
2023-02-15 10:59:18 +0300

    God bless 206th commit! --- new login button

--- eye/pages/login.vue
+++ eye/pages/login.vue
@@ -13,12 +13,7 @@
 <template>
   <section class="content">
     <h3 class="is-size-5">{{ $t('title') }}</h3>
-    <script async src="https://telegram.org/js/telegram-widget.js?21" data-telegram-login="ArtchitectBot" data-size="medium" data-onauth="onTelegramAuth(user)" data-request-access="write"></script>
-    <script type="text/javascript">
-      function onTelegramAuth(user) {
-        alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
-      }
-    </script>
+    <script async src="https://telegram.org/js/telegram-widget.js?21" data-telegram-login="ArtchitectBot" data-size="large" data-auth-url="https://artchitect.space/api/login" data-request-access="write"></script>
   </section>
 </template>
 
