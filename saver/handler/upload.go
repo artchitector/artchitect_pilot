d4ba30fd59823f5a28b679064b857f21e8fb02a2
artchitector <i@nkuhta.ru>
2023-02-17 01:04:01 +0300

    God bless 219th commit! --- logs

--- saver/handler/upload.go
+++ saver/handler/upload.go
@@ -31,16 +31,19 @@ func (h *UploadHandler) Handle(c *gin.Context) {
 	// single file
 	file, err := c.FormFile("file")
 	if err != nil {
+		log.Error().Err(err).Msgf("[upload:card] failed to get file")
 		c.String(http.StatusBadRequest, errors.Wrap(err, "[saver_upload] failed to get file from form").Error())
 		return
 	}
 	cardIdStr := c.PostForm("card_id")
 	if cardIdStr == "" {
+		log.Error().Msgf("[upload:card] card_id must be integer")
 		c.String(http.StatusBadRequest, "card_id must be integer")
 		return
 	}
 	cardID, err := strconv.Atoi(cardIdStr)
 	if err != nil {
+		log.Error().Msgf("[upload:card] card_id must be integer")
 		c.String(http.StatusBadRequest, err.Error())
 		return
 	}
@@ -49,17 +52,20 @@ func (h *UploadHandler) Handle(c *gin.Context) {
 
 	f, err := file.Open()
 	if err != nil {
+		log.Error().Err(err).Msgf("[upload:card] failed to to open file")
 		c.String(http.StatusInternalServerError, err.Error())
 		return
 	}
 
 	data, err := io.ReadAll(f)
 	if err != nil {
+		log.Error().Err(err).Msgf("[upload:card] failed to io readAll")
 		c.String(http.StatusInternalServerError, err.Error())
 		return
 	}
 
 	if err := h.saver.SaveImage(uint(cardID), data); err != nil {
+		log.Error().Err(err).Msgf("[upload:card] failed SaveImage card_id=%d", cardID)
 		c.String(http.StatusInternalServerError, err.Error())
 		return
 	}
@@ -71,27 +77,32 @@ func (h *UploadHandler) HandleHundred(c *gin.Context) {
 	// single file
 	file, err := c.FormFile("file")
 	if err != nil {
+		log.Error().Err(err).Msgf("[upload:hundred] failed to get file")
 		c.String(http.StatusBadRequest, errors.Wrap(err, "[saver_upload] failed to get file from form").Error())
 		return
 	}
 	rankStr := c.PostForm("rank")
 	if rankStr == "" {
-		c.String(http.StatusBadRequest, "card_id must be integer")
+		log.Error().Msgf("[upload:hundred] rank must be integer")
+		c.String(http.StatusBadRequest, "rank must be integer")
 		return
 	}
 	rank, err := strconv.Atoi(rankStr)
 	if err != nil {
+		log.Error().Msgf("[upload:hundred] rank must be integer")
 		c.String(http.StatusBadRequest, err.Error())
 		return
 	}
 
 	hundredStr := c.PostForm("hundred")
 	if hundredStr == "" {
-		c.String(http.StatusBadRequest, "card_id must be integer")
+		log.Error().Msgf("[upload:hundred] hundred must be integer")
+		c.String(http.StatusBadRequest, "hundred must be integer")
 		return
 	}
 	hundred, err := strconv.Atoi(hundredStr)
 	if err != nil {
+		log.Error().Msgf("[upload:hundred] hundred must be integer")
 		c.String(http.StatusBadRequest, err.Error())
 		return
 	}
@@ -100,17 +111,20 @@ func (h *UploadHandler) HandleHundred(c *gin.Context) {
 
 	f, err := file.Open()
 	if err != nil {
+		log.Error().Err(err).Msgf("[upload:hundred] failed to open file")
 		c.String(http.StatusInternalServerError, err.Error())
 		return
 	}
 
 	data, err := io.ReadAll(f)
 	if err != nil {
+		log.Error().Err(err).Msgf("[upload:hundred] failed to io.ReadAll")
 		c.String(http.StatusInternalServerError, err.Error())
 		return
 	}
 
 	if err := h.saver.SaveHundredImage(uint(rank), uint(hundred), data); err != nil {
+		log.Error().Err(err).Msgf("[upload:hundred] failed to SaveHundredImage r:%d h:%d", rank, hundred)
 		c.String(http.StatusInternalServerError, err.Error())
 		return
 	}
