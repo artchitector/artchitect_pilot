6e49b13063d03b9ddfd4a203974b43e445390b72
artchitector <i@nkuhta.ru>
2023-03-10 14:49:31 +0300

    God bless 272th commit! --- unity tg text

--- bot/text.go
+++ bot/text.go
@@ -52,7 +52,7 @@ func getUnityText(unity model.Unity) (string, error) {
 	}
 	isCompleted := totalLeads == filledLeads
 	return fmt.Sprintf(
-		"Unity %s (re)unified. Version %d. Leads: %d from %d. Completed: %t. https://artchitect.space/unity/%s",
+		"Unity %s (re)unified. Version %d.\n Leads: %d from %d. Completed: %t.\n\n https://artchitect.space/unity/%s",
 		unity.Mask,
 		unity.Version,
 		filledLeads,
