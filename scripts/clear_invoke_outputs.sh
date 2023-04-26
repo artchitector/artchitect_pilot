9eb1465595d2b4470cb06bc37ec969590a3b0c3f
artchitector <i@nkuhta.ru>
2023-04-26 11:18:53 +0300

    God bless 291th commit! --- clear outputs in invoke.AI

--- scripts/clear_invoke_outputs.sh
+++ scripts/clear_invoke_outputs.sh
@@ -1,2 +1,2 @@
 # delete old png images from outputs folder
-find /home/artchitector/invoke-ai/invokeai_v2.2.5/invokeai/outputs/*.png -daystart -mtime +1 -delete
+find /home/artchitector/invoke-ai/invokeai_v2.3.0/outputs/*.png -daystart -mtime +1 -delete
