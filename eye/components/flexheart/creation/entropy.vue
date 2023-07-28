3cd1d3ad01e0d6b4850bd0499d5f6620ddcb5089
artchitector <i@nkuhta.ru>
2023-07-28 17:05:06 +0300

    ❤️ bless 352th commit! --- entropy component

---
+++ eye/components/flexheart/creation/entropy.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="has-text-centered">
+    <span class="is-size-7">ENTROPY</span><br/>
+    <img v-if="entropy.ImagesEncoded.entropy !== null" :src="`data:image/png;base64, ${entropy.ImagesEncoded.entropy}`"
+         style="width: 64px; height: 64px; image-rendering: pixelated;" alt="loading entropy stream"/>
+    <img v-if="entropy.ImagesEncoded.choice !== null" :src="`data:image/png;base64, ${entropy.ImagesEncoded.choice}`"
+         style="width: 64px; height: 64px; image-rendering: pixelated;" alt="loading choice stream"/>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['entropy']
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
